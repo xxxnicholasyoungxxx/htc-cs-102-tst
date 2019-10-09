9abc00dd51c81cb0a18d7bee563f44d491892e16
NicholasYoung <nyoung22@midviewk12.org>
2019-10-09 19:31:10 -0400

    Made textboxes andcreated class, just copy instructor

---
+++ Week03/MovieApplication/MovieApplication/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MovieApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Movie submittedMovie = new Movie();
+
+            submittedMovie.Title = titleTextBox1.Text;
+            submittedMovie.Genre = genreTextBox.Text;
+            submittedMovie.ReviewScore = Double.Parse(reviewScoreTextBox.Text);
+        }
+    }
+}
